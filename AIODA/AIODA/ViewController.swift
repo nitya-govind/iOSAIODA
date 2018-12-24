bedf82acde5a1e29524ddf8ce6fddd281c551d54
Nitya Govind <nityagovind@Vos-iMac.local>
2018-12-23 19:02:40 -0800

    1. Basic outline of an iOS app added 2. Added a temporary AIODA image as icon for the app

---
+++ AIODA/AIODA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AIODA
+//
+//  Created by Nitya Govind on 12/16/18.
+//  Copyright © 2018 Nitya Govind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
